abaa6d7656655193555b55e89f8713c72b5a205c
ignacio ricart <imr@powerstow.com>
2021-02-18 17:11:28 +0100

    add function object to part1

--- src/part1.cpp
+++ src/part1.cpp
@@ -8,21 +8,24 @@ public:
     {
         for (int i = 0; i < 2; ++i)
         {
-            std::cout << "Inside thread function" << std::endl;
+            std::cout << "Inside thread function from DisplayThread object..." << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
 };
 
+// Class able to use call a thread using Function Pointer, Function Object, and Lambda Function as constructor
 class ThreadPart1
 {
 private:
     std::thread my_thread;
 public:
-    thread_part1()
+    ThreadPart1()
     {
-        std::cout << "Constructor" << std::endl;
+        std::cout << "Constructor..." << std::endl;
     }
+
+    // Had to add the `static` to the function
     static void static_thread_function()
     {
         for (int i = 0; i < 2; ++i)
@@ -56,6 +59,11 @@ public:
             }});
     }
 
+    void construct_thread_function_object()
+    {
+        this->my_thread = std::thread(DisplayThread());
+    }
+
     void join()
     {
         my_thread.join();
@@ -64,8 +72,19 @@ public:
 
 int main()
 {
-    thread_part1 my_thread_exercise;
+    ThreadPart1 my_thread_exercise;
     my_thread_exercise.construct_thread_function_static();
     my_thread_exercise.join();
+
+    // Using Lambda
+    std::cout << "Now using lambda..." << std::endl;
+    my_thread_exercise.construct_thread_function_lambda();
+    my_thread_exercise.join();
+
+    // Using Function Object
+    std::cout << "Now using function object..." << std::endl;
+    my_thread_exercise.construct_thread_function_object();
+    my_thread_exercise.join();
+
     return 0;
 }
